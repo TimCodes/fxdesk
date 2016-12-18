6f67c3b9d30741931842f6f32b4f29e6e8db2a00
Admin <Admin@administrators-MacBook-Air.local>
2016-12-17 22:59:59 -0700

    protoyped trade tracker

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { Router, Route, hashHistory, IndexRoute} from 'react-router';
 import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Container } from 'semantic-ui-react'
 
-
 import CandleStickChart from './candlestick'
-
 import ChartSidebar from './components/ChartSidebar';
-
+import TradeTracker from './components/TradeTracker';
 
 import logo from './logo.svg';
 import './App.css';
@@ -14,12 +12,13 @@ import './App.css';
 
 
 class App extends Component {
-  state = { visible: false }
+ 
   constructor(props) {
     super(props);
 
     this.state = {
-      data:  []
+      data:  [],
+      visible: false
       
     };
   }
@@ -32,31 +31,31 @@ class App extends Component {
   render() {
     const { visible } = this.state
     return (
-        <div className ="basic minimal pushable">
+        <div className ="ui basic minimal pushable"  onClick={this.dimVisibility}>
           <ChartSidebar   visible={this.state.visible} />
-         <div className = "ui container fluid "  onClick={this.dimVisibility}> 
-          <div className="ui black small launch right attached fixed button" onClick={this.toggleVisibility}>
-          <i className="content icon"></i>
-          
-        </div>
-      
-          <CandleStickChart data={this.props.data} type={'hybrid'} />
-         </div>
-       </div>
+          <div className = "ui container fluid " > 
+            <div className="ui black small launch right attached fixed button" onClick={this.toggleVisibility}>
+              <i className="content icon"></i>
+            </div>
+          </div>
+          <div className = "ui container">
+           <TradeTracker />
+          </div> 
+        </div>    
     )
   }
 }
 
+// <Route path="/" component={Main}>
+//             <Route path="profile/:username" component={Profile} />
+//             <IndexRoute component={Home} />
+//           </Route>
+//           <CandleStickChart data={this.props.data} type={'hybrid'} />
 
 function chartWrapper(data ){
 
 
 }
-
-  // <Route path="/" component={Main}>
-        //     <Route path="profile/:username" component={Profile} />
-        //     <IndexRoute component={Home} />
-        //   </Route>
 
 export default App;
 

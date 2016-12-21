92d6658c31a0d5ad4ad116db20daf0f2cfd2485f
Tim Hardy <thardy@sbt.local>
2016-12-20 17:11:28 -0700

    workedo nform

--- src/components/TradeTracker.js
+++ src/components/TradeTracker.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Card, Image, Segment, Flag, Dropdown, Grid } from 'semantic-ui-react'
+import { Button, Card, Image, Segment, Flag, Dropdown, Grid, Header, Icon, Modal, Form, Checkbox } from 'semantic-ui-react'
+import Dropzone from 'react-dropzone';
 
 
 const trigger = (
@@ -13,31 +14,44 @@ class TradeTracker extends Component {
         super();
      this.state = {
         data:  [1,2,3,4],
-        visible: true
-      
+        visible: true,
+        addModalVis : false
      };
+
+     this.showModalVis = this.showModalVis.bind(this)
+    }
+
+    showModalVis(){
+        console.log("set mdal vias")
+        this.setState({
+            addModalVis: true
+        })
     }
 
     render(){ return (
       <div className = "ui container" >
-
+        <Header as='h1' color = "pink" textAlign = 'center'>
+            Trade Tracker  
+            
+        </Header>
+        <Segment floated = 'right' circular compact  onClick = {this.showModalVis}> 
+                <Icon name='plus'></Icon>
+        </Segment>
+        
         <Grid>
           {this.state.data.map( (e, idx) => {
             return (   
-          <Grid.Column  computer={5} mobile = {9}>      
+          <Grid.Column  computer={5} mobile = {12}>      
           <Card color='green' key = {idx}  fluid className = "blue-bg"> 
             <Card.Content >
-               
-                  <Dropdown  inline trigger={trigger} icon  null className ='right floated' pointing = 'left top'>
+                <Dropdown  inline trigger={trigger} icon  className ='right floated' pointing = 'left top'>
                     <Dropdown.Menu>
-                    <Dropdown.Item text='New' />
-                    <Dropdown.Item text='Open...' description='ctrl + o' />
-                    <Dropdown.Item text='Save as...' description='ctrl + s' />
-
-                    <Dropdown.Item text='Rename' description='ctrl + r' />
+                    <Dropdown.Item text='View' description = 'view details' />
+                    <Dropdown.Item text='Edit' description='edit details' />
+                    <Dropdown.Item text='Close' description='close to history' />
+                    <Dropdown.Item text='Delete' description='delete for good' />
                     </Dropdown.Menu>
                 </Dropdown>
-                
                 <Card.Header>
                 EURUSD
                 </Card.Header>
@@ -55,6 +69,36 @@ class TradeTracker extends Component {
             )
           })}
          </Grid>
+         
+         <Modal open = {this.state.addModalVis}>
+            <Modal.Header>Track New Trade</Modal.Header>
+            <Modal.Content >
+              <Form>
+                    <Form.Field>
+                    <label>Pair</label>
+                    <input placeholder='EURUSD' />
+                    </Form.Field>
+                    <Form.Field>
+                    <label>Status</label>
+                    <input placeholder='Tracking' />
+                    </Form.Field>
+                    <Form.Field>
+                    <label>Setup Type</label>
+                    <input placeholder='Goodman' />
+                    </Form.Field>
+                    <Form.Field>
+                    <label>Side</label>
+                    <input placeholder='Buy' />
+                    </Form.Field>
+                    <Form.TextArea label='Description' placeholder='Tell us more about you...' />
+                    <Dropzone >
+                        <div>Try dropping some files here, or click to select files to upload.</div>
+                    </Dropzone>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+
+            </Modal.Content>
+        </Modal>
        
        </div> 
     )    
